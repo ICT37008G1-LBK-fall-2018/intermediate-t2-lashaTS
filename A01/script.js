e23244b2124d0486fd848501403de905828b5fc8
nika <gabodzenika@gmail.com>
2018-12-05 17:54:08 +0400

    a01

---
+++ A01/script.js
@@ -0,0 +1,13 @@
+let nums =prompt('enter 2number').split(' '),
+a = +nums[0],
+b = +nums[1];
+sum = 0;
+
+if ((a <= 0 || b <= 0) || (a >= 1000 || b >= 1000) || a >=b ){
+    alert('არასწორი შუალედი');
+} else{
+    for (a; a<=b; a++){
+        sum += a;
+    }
+    alert (sum);
+}
